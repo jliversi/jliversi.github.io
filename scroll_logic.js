86a1be25b636aa327a8363b88f5d2324ccc9042d
Joshua Ashmall-Liversidge <jliversi.dev@gmail.com>
2023-08-23 11:21:19 -0400

    Temp remove projects

--- scroll_logic.js
+++ scroll_logic.js
@@ -12,30 +12,44 @@ const projects = document.getElementById("projects");
 let aboutHeight = about.scrollHeight;
 let skillsHeight = skills.scrollHeight;
 let experienceHeight = experience.scrollHeight;
-let projectsHeight = projects.scrollHeight;
+// let projectsHeight = projects.scrollHeight;
 window.addEventListener('resize', () => {
   aboutHeight = about.scrollHeight;
   skillsHeight = skills.scrollHeight;
   experienceHeight = experience.scrollHeight;
-  projectsHeight = projects.scrollHeight;
+  // projectsHeight = projects.scrollHeight;
 });
 
 
-const sections = ['about', 'skills', 'experience', 'projects'];
+// const sections = ['about', 'skills', 'experience', 'projects'];
+const sections = ['about', 'skills', 'experience'];
+
+// function determineSectionIdx() {
+//   const currentScroll = content.scrollTop;
+//   if (currentScroll <  aboutHeight) {
+//     return 0;
+//   } else if (currentScroll < (aboutHeight + skillsHeight)) {
+//     return 1;
+//   } else if (currentScroll < (aboutHeight + skillsHeight + experienceHeight)) {
+//     if (Math.floor(window.innerHeight + content.scrollTop) === content.scrollHeight) {
+//       return 3;
+//     }
+//     return 2;
+//   }
+//   return 3;
+// }
 
 function determineSectionIdx() {
   const currentScroll = content.scrollTop;
   if (currentScroll <  aboutHeight) {
     return 0;
   } else if (currentScroll < (aboutHeight + skillsHeight)) {
-    return 1;
-  } else if (currentScroll < (aboutHeight + skillsHeight + experienceHeight)) {
     if (Math.floor(window.innerHeight + content.scrollTop) === content.scrollHeight) {
-      return 3;
+      return 2;
     }
-    return 2;
+    return 1;
   }
-  return 3;
+  return 2;
 }
 
 let sectionIdx = 0;
@@ -60,7 +74,7 @@ function changeSectionBox() {
 const about_btn = document.getElementById("about-btn");
 const skills_btn = document.getElementById("skills-btn");
 const experience_btn = document.getElementById("experience-btn");
-const projects_btn = document.getElementById("projects-btn");
+// const projects_btn = document.getElementById("projects-btn");
 
 about_btn.addEventListener('click', () => {
   content.scrollTop = 0;
@@ -71,7 +85,7 @@ skills_btn.addEventListener('click', () => {
 experience_btn.addEventListener('click', () => {
   content.scrollTop = aboutHeight + skillsHeight;
 });
-projects_btn.addEventListener('click', () => {
-  content.scrollTop = aboutHeight + skillsHeight + experienceHeight;
-});
+// projects_btn.addEventListener('click', () => {
+//   content.scrollTop = aboutHeight + skillsHeight + experienceHeight;
+// });
 
